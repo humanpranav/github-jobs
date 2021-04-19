3d913551f7ca515cafebb9154ac93586d6fbb1dd
Pranav <68323814+humanpranav@users.noreply.github.com>
2021-04-19 10:15:10 -0700

    changing fetch jobs for production

--- src/fetchJobs.js
+++ src/fetchJobs.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+const BASE_URL = "https://jobs.github.com/positions.json";
 
 export const retrieveData = async () => {
   let response = await axios.get("https://jobs.github.com/positions.json");
