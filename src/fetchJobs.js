c08b97de6bc74ff858901154fca626bec9999fdf
Pranav <68323814+humanpranav@users.noreply.github.com>
2021-04-19 11:27:56 -0700

    changing fetch jobs to json for cors error

--- src/fetchJobs.js
+++ src/fetchJobs.js
@@ -1,11 +1,19 @@
 import axios from "axios";
+import data from "./response.json";
+// const BASE_URL =
+//   "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
 
-const BASE_URL = "https://jobs.github.com/positions.json";
+// export const retrieveData = async () => {
+//   let response = await axios.get("https://jobs.github.com/positions.json");
 
+//   console.log(response);
+
+//   return response.data;
+// };
 export const retrieveData = async () => {
-  let response = await axios.get("https://jobs.github.com/positions.json");
+  // let response = await axios.get("response.json");
 
-  console.log(response);
+  console.log(data);
 
-  return response.data;
+  return data;
 };
